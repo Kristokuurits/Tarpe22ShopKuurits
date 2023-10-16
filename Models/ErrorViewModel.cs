a242e490d1f29e433187d4129248bd0d0ddfe9e6
Opilane TTHK <opilane@tthk.ee>
2023-10-16 08:59:13 +0300

    mhm

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarpe22ShopKuurits.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
